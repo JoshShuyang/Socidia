57c2bb8eb5f0f9721fa546e012258a4e8973bc08
na-yue <na.yue@sjsu.edu>
2019-04-17 14:47:49 -0700

    replaced deprecated WebMvcConfigurerAdapter calss

--- SocialMediaProtection/src/main/java/socialmediaprotection/project/config/CORSConfig.java
+++ SocialMediaProtection/src/main/java/socialmediaprotection/project/config/CORSConfig.java
@@ -3,11 +3,12 @@ package socialmediaprotection.project.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class CORSConfig extends WebMvcConfigurerAdapter {
+public class CORSConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,6 +17,7 @@ public class CORSConfig extends WebMvcConfigurerAdapter {
                 .allowedMethods("*")
                 //.allowedHeaders("Content-Type", "Authorization")
                 //.allowCredentials(false)
-                .maxAge(32400);  // 9 hours max age
+                //.maxAge(32400)  // 9 hours max age
+                ;
     }
 }
